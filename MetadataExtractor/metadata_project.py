b5ef6d020373233d684b38762264c7868acf1919
Alex Birkett <ab1426@bristol.ac.uk>
2023-12-18 17:28:40 +0000

    first version with main

--- MetadataExtractor/metadata_project.py
+++ MetadataExtractor/metadata_project.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 # Function to prompt for project metadata and return it as a dictionary
 def get_project_metadata():
@@ -17,16 +18,25 @@ def get_project_metadata():
         'PROJECT_COPYRIGHT': input("Enter the PROJECT_COPYRIGHT: "),
     }
 
+# Prompt for the directory to save the output CSV file
+save_directory = input("Enter the directory where you want to save the CSV file (e.g., C:/Documents/): ")
+if not os.path.isdir(save_directory):
+    print("Directory does not exist. Creating the directory.")
+    os.makedirs(save_directory, exist_ok=True)
+
 # Prompt for the output CSV file name
-output_csv = input("Enter the output CSV file name (e.g., project_metadata.csv): ")
+output_csv_name = input("Enter the output CSV file name (e.g., project_metadata.csv): ")
+
+# Full path for the output CSV file
+output_csv_path = os.path.join(save_directory, output_csv_name)
 
 # Get project metadata from the user
 project_metadata = get_project_metadata()
 
 # Write the project metadata to a CSV file
-with open(output_csv, 'w', newline='') as csvfile:
+with open(output_csv_path, 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=project_metadata.keys())
     writer.writeheader()
     writer.writerow(project_metadata)
 
-print(f"Project metadata has been saved to {output_csv}")
+print(f"Project metadata has been saved to {output_csv_path}")
